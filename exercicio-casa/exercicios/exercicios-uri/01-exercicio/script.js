5eec5957d758535f2e7ccafad81b3adeb1f69031
Viviane Miranda <viviane.miranda@nubank.com.br>
2021-04-14 21:51:04 -0300

    solving loop and conditional exercises

--- exercicio-casa/exercicios/exercicios-uri/01-exercicio/script.js
+++ exercicio-casa/exercicios/exercicios-uri/01-exercicio/script.js
@@ -1,1 +1,11 @@
 // [Exercício 01](https://www.urionlinejudge.com.br/judge/pt/problems/view/1038) 
+
+const codigoProduto = Number(prompt('digite o codigo do produto 1-5'));
+const quantidadeProduto = Number(prompt('quantas unidades do produto voce comprou?'));
+
+function valorConta(a,b) {
+    let valorFinal =  parseFloat(a*b);
+    return alert(`Total: R$ ${valorFinal}`)
+};
+
+valorconta(codigoProduto, quantidadeProduto);
